5bb13b73874a86389c3c46c69d39e2cec62442e1
Geraldo_Neto <57219095+Geraldo-NetoLPG4@users.noreply.github.com>
2020-03-29 14:54:59 -0300

    Master Project

---
+++ Projeto Ra 001/Assets/Coisas do Projeto Ra/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    public float playerspeed;
+
+    Vector3 playeraxis;
+    public CharacterController cctrl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playeraxis = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        cctrl.SimpleMove(transform.TransformDirection(playeraxis * playerspeed));
+
+    }
+}
