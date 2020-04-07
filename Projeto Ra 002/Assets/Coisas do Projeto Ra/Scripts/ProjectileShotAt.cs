f4dcfbadf2e5144f2fff472aabc8dedd40d894bb
Geraldo_Neto <57219095+Geraldo-NetoLPG4@users.noreply.github.com>
2020-04-07 14:20:49 -0300

    Apresentacao 07/04

---
+++ Projeto Ra 002/Assets/Coisas do Projeto Ra/Scripts/ProjectileShotAt.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileShotAt : MonoBehaviour
+{
+    public GameObject begone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Shot")
+        {
+            begone.GetComponent<Renderer>().enabled = false;
+            begone.GetComponent<Collider>().enabled = false;
+        }
+    }
+
+
+}
+
---
+++ Projeto Ra 002/Assets/Coisas do Projeto Ra/Scripts/SpawnEnemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    public float maxEnemies;
+
+    public GameObject enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.FindGameObjectsWithTag("Zumbi").Length < maxEnemies)
+        {
+            Instantiate(enemy, new Vector3(0, 0, 0), Quaternion.identity);//alterar o "0, 0, 0," pra posição que vc quer
+            //yield return new WaitForSeconds(1.0f);//se isso não funcionar, só tira
+
+        }
+    }
+
+
+
+}
+
