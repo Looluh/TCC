1ce92a03c1d210dcaf1d9dfe8aaa809a86a301ea
Geraldo_Neto <57219095+Geraldo-NetoLPG4@users.noreply.github.com>
2020-05-08 18:45:49 -0300

    Revert "Teleshot, Teleswap"
    This reverts commit fc3e9bf937974a08e5827a3eb3ed729ed1fb99de.

---
+++ Projeto Ra 002/Assets/Scripts/TeleshotEnemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleshotEnemy : MonoBehaviour
+{
+    public Transform player;
+    public Transform myself;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ainda não funciona. n faço a minima ideia do pq.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Shot"))
+        {
+            player.GetComponent<CharacterController>().enabled = false;
+            player.transform.position = myself.transform.position;
+            player.GetComponent<CharacterController>().enabled = true;
+            //Destroy(gameObject);
+        }
+    }
+}
